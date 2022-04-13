bbdc6111a99c082a5382262ef02bf9fa9b41a85b
Jordan Haynes <Jordancautious@gmail.com>
2022-04-13 13:25:44 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jordan Haynes on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
